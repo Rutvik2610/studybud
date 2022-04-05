964262b1ba44f6cb37a6071112a767a4470ffa76
Rutvik2610 <rutvikpurohit10@gmail.com>
2022-04-06 01:28:52 +0530

    commit on 'master'

--- studybud/settings.py
+++ studybud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9tr4*7457tm0e*fa66e2=88_7yrkht(^)f1e%8nu0v@nst-&r7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
